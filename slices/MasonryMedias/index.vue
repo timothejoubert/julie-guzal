462293a6549e71d2c996e6adf5f917a200aa098c
timothe <timothe@rezo-zero.com>
2025-02-05 21:09:30 +0100

    style(MasonryMedias): add offset top for second column

--- slices/MasonryMedias/index.vue
+++ slices/MasonryMedias/index.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
 import type { MasonryMediasSlice } from '~/prismicio-types'
+import { getHtmlElement, type TemplateElement } from '~/utils/ref/get-html-element'
+
+type StoredImage = { el: null | HTMLElement, top: null | number, index: number }
 
 const props = defineProps(getSliceComponentProps<MasonryMediasSlice>())
 const primary = computed(() => props.slice.primary)
+
+const mediaInstances = ref<TemplateElement[]>([])
+const imageElementList = computed(() => mediaInstances.value.map(instance => getHtmlElement(instance)))
+const lastTopColumnedElement = computed(() => {
+    return imageElementList.value.reduce((acc, el, index) => {
+        const top = el?.getBoundingClientRect().top
+
+        if (!acc.el || (top && acc?.top && top <= acc?.top)) acc = { el, top, index } as StoredImage
+        return acc
+    }, { el: null, top: null, index: -1 } as StoredImage)
+})
+
+const lastTopColumnedElementIndex = computed(() => lastTopColumnedElement.value.index)
 </script>
 
 <template>
@@ -14,7 +30,8 @@ const primary = computed(() => props.slice.primary)
             <VPrismicMedia
                 v-for="(field, index) in primary.list"
                 :key="index"
-                :class="$style.image"
+                ref="mediaInstances"
+                :class="[$style.image, index === lastTopColumnedElementIndex && $style['image--offset-top']]"
                 :document="field.image"
                 :image="{ sizes: 'xs:100vw sm:100vw md:100vw lg:50vw xl:50vw xxl:50vw hq:50vw qhd:50vw' }"
             />
@@ -41,6 +58,11 @@ const primary = computed(() => props.slice.primary)
 
     @include media('>=lg') {
         margin-bottom: rem(24);
+        transition: padding-top 0.4s ease(out-quad);
+
+        &--offset-top {
+            padding-top: rem(316);
+        }
     }
 }
 </style>
