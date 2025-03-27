9e21e87ba0660197cd9353b8862ebd54e05122de
timothe <timothe@rezo-zero.com>
2025-03-27 19:22:52 +0100

    perf: discreet smooth scroll with prefers-reduce-motion and on touch

--- plugins/lenis.client.ts
+++ plugins/lenis.client.ts
@@ -2,9 +2,16 @@ import Lenis from 'lenis'
 
 // TODO: disable if prefersReduceMotion is enable
 export default defineNuxtPlugin((_nuxtApp) => {
-    const lenis = new Lenis()
+    const lenis = new Lenis({
+        touchMultiplier: 0,
+    })
 
     if (import.meta.client) {
+        const reduceMotion = !!window?.matchMedia(`(prefers-reduced-motion: reduce)`).matches
+
+        lenis.options.wheelMultiplier = reduceMotion ? 0 : 1
+        lenis.options.lerp = reduceMotion ? 1 : 0.08
+
         function raf(time: DOMHighResTimeStamp) {
             lenis.raf(time)
             requestAnimationFrame(raf)
