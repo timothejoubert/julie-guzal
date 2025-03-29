71b35d7ed9868befef77fe3af02fded3d151e493
timothe joubert <timothejoubert26@gmail.com>
2025-03-29 15:28:59 +0100

    chore: lint fix

--- server/api/run-github-actions-deploy.post.ts
+++ server/api/run-github-actions-deploy.post.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler((event) => {
-  console.log('api route', event);
-  
+    console.log('api route', event)
+
     return {
-      hello: 'world'
+        hello: 'world',
     }
-  })
+})
