ea9b63e89701967a9c03ee02d21c32168f2df2e7
timothe joubert <timothejoubert26@gmail.com>
2025-03-31 21:52:34 +0200

    fix: fix preview page content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,23 @@
 <script  lang="ts" setup>
 import { getDocumentTypeByUrl } from '~/utils/prismic/route-resolver'
-import { usePrismicPreviewRoute } from '~/composables/use-prismic-preview-route'
 
-const route = useRoute()
-const { isPreview } = usePrismicPreviewRoute()
 
 // Set current page for component data outside pages
-const isWildCardPage = route.matched?.find(match => match.path === '/:uid(.*)*')
-if (isWildCardPage || isPreview) {
-    const pageType = isPreview ? 'preview' : getDocumentTypeByUrl(route.path)
+callOnce(async () => {
+    const route = useRoute()
+    const isWildCardPage = route.matched?.find(match => match.path === '/:uid(.*)*')
+    
+    if (isWildCardPage) {
+        const pageType = getDocumentTypeByUrl(route.path)
 
-    if (pageType) {
-        const { webResponse } = await usePrismicFetchPage(pageType)
-        useCurrentPage({ webResponse })
+        if (pageType) {
+            const { webResponse } = await usePrismicFetchPage(pageType)
+            useCurrentPage({ webResponse })
+        }
     }
-}
+})
 
 const appConfig = useAppConfig()
-
-// const { data, error } = await useFetch('/api/run-github-actions-deploy', {
-//     method: 'post',
-// })
-
-// console.log('post fetch response', data.value);
-// console.log('post fetch error', error.value);
 </script>
 
 <template>
