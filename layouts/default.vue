9b11fe6e5750eefd2f660d5afd6d4ac2eb172bea
timothe joubert <timothejoubert26@gmail.com>
2025-07-01 19:15:07 +0200

    chore: update .env and add comment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,17 @@
 <script  lang="ts" setup>
 import { getDocumentTypeByUrl } from '~/utils/prismic/route-resolver'
 
-// Set current page for component data outside pages
-// callOnce(async () => {
 const route = useRoute()
 const isWildCardPage = route.matched?.find(match => match.path === '/:uid(.*)*')
+
 if (isWildCardPage) {
+    // Set current page for component data outside pages
     const pageType = getDocumentTypeByUrl(route.path)
 
     const { webResponse } = await usePrismicFetchPage(pageType)
     if (webResponse) useCurrentPage().value = { webResponse }
 }
-// })
 
-console.log('default layout')
 const appConfig = useAppConfig()
 </script>
 
